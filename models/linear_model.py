07c6884c043a010acb4e7a8397370999ab3da47d
Muskaan-Adil <muskaanadil7@gmail.com>
2025-04-26 20:25:32 +0500

    Update linear_model.py

--- models/linear_model.py
+++ models/linear_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 import logging
-from src.models.base_model import BaseModel
+from models.base_model import BaseModel
 
 logger = logging.getLogger(__name__)
 
