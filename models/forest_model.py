09c6bf0751cc852d7231a5711bce24657e297f5d
Muskaan-Adil <muskaanadil7@gmail.com>
2025-04-26 20:25:51 +0500

    Update forest_model.py

--- models/forest_model.py
+++ models/forest_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import logging
-from src.models.base_model import BaseModel
+from models.base_model import BaseModel
 
 logger = logging.getLogger(__name__)
 
